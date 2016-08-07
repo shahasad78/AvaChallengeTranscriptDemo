983232b5e951fe56f3847830cb0dd46c36957b92
Richard Martinez <shahasad78@gmail.com>
2016-08-07 15:59:42 -0700

    Initial Commit

---
+++ AvaChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AvaChallenge
+//
+//  Created by Richard Martinez on 8/7/16.
+//  Copyright © 2016 PhantomUniversalMediaProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
