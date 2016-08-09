19a8360652e3522efd4d33505137c78b3703ef00
Richard Martinez <shahasad78@gmail.com>
2016-08-09 05:05:40 -0700

    Working transcript subscription. removed separator bars from cells.

---
+++ AvaChallenge/Helpers and Extensions/AvaMessage+TableView.swift
@@ -0,0 +1,9 @@
+//
+//  AvaMessage+TableView.swift
+//  AvaChallenge
+//
+//  Created by Richard Martinez on 8/8/16.
+//  Copyright © 2016 PhantomUniversalMediaProductions. All rights reserved.
+//
+
+import Foundation
---
+++ AvaChallenge/Helpers and Extensions/Bool+Int.swift
@@ -0,0 +1,6 @@
+//
+// Created by Richard Martinez on 8/8/16.
+// Copyright (c) 2016 PhantomUniversalMediaProductions. All rights reserved.
+//
+
+import Foundation
